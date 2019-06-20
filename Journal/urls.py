2bac5a316b80d13994d3139cc412b3adc977ca26
Mohammad Azimi <Mohammad.azimi.1996@gmail.com>
2019-06-20 23:39:13 +0430

    new static files

--- Journal/urls.py
+++ Journal/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url , include
 from django.contrib import admin
-from Journal import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$' , views.index )
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
+    url(r'^index$' , include('Journal.urls'))
+]
