2a1497607cec201d7b7afa47d6b6564eec11b808
Mohammad Azimi <Mohammad.azimi.1996@gmail.com>
2019-06-23 16:19:57 +0430

    now we have issues and volumes from db

--- Journal/views.py
+++ Journal/views.py
@@ -6,6 +6,10 @@ from . import models
 # Create your views here.
 
 def index(request):
-    years = models.YearRange.objects.all()
+    years = models.YearRange.objects.all().order_by('-low_year')
+    for year in years:
+        year.Volumes = year.volumes_set.all().order_by('-volume_number')
+        for volume in year.Volumes:
+            volume.issue = volume.issues_set.all().order_by('-issue_number')
     context = {'years' : years} 
     return render(request , "Journal/index.html" , context)
